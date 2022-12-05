2d073934a4da4a07219d837a73b1838c4d968e9e
Johannes Ballmann <574988+fibsifan@users.noreply.github.com>
2022-12-05 23:27:41 +0100

    Day05 Part 2

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -1,32 +1,51 @@
 import java.util.Stack
 
 class Day05(test: Boolean = false): Day<String>(test, "CMZ", "MCD") {
-    private val crates: List<Stack<Char>>
+    private val crates1: List<Stack<Char>>
+    private val crates2: List<Stack<Char>>
 
     init {
-        val tmp = input.subList(0,input.indexOf("")-1)
+        val crateNumbers = input[input.indexOf("") - 1]
+            .split(" ")
+            .filter { it.isNotBlank() }
+            .map { it.toInt() }
+        val crateLines = input.subList(0,input.indexOf("")-1)
             .map { line ->
                 line.chunked(4)
                     .map { crate -> crate.trim() }
                     .map { crate -> if (crate.matches("\\[\\w]".toRegex())) crate[1] else null }
             }
-        crates = tmp[0].map { Stack<Char>() }
-        tmp.reversed().forEach { list -> list.forEachIndexed { index, crate -> if (crate != null) crates[index].push(crate) }}
+        crates1 = crateNumbers.map { Stack<Char>() }
+        crates2 = crateNumbers.map { Stack<Char>() }
+        crateLines.reversed()
+            .forEach { list -> list
+                .forEachIndexed { index, crate ->
+                    if (crate != null) {
+                        crates1[index].push(crate)
+                        crates2[index].push(crate)
+                    }
+                }
+            }
     }
 
     private val instructions = input.subList(input.indexOf("")+1, input.size)
         .map { Regex("move (\\d+) from (\\d+) to (\\d+)").find(it)!!.groups }
+        .map { groups -> (1..3).map { match -> groups[match]!!.value.toInt() } }
+
     override fun part1(): String {
-        instructions.forEach { instruction -> repeat(getNumber(instruction, 1)) {
-            crates[getNumber(instruction, 3)-1].push(crates[getNumber(instruction, 2)-1].pop())
+        instructions.forEach { instruction -> repeat(instruction[0]) {
+            crates1[instruction[2]-1].push(crates1[instruction[1]-1].pop())
         } }
-        return crates.map { it.pop() }.joinToString("")
+        return crates1.map { it.pop() }.joinToString("")
     }
 
-    private fun getNumber(instruction: MatchGroupCollection, position: Int) = instruction[position]!!.value.toInt()
-
     override fun part2(): String {
-        TODO("Not yet implemented")
+        instructions.forEach { instruction -> (1..instruction[0])
+            .map { crates2[instruction[1]-1].pop() }
+            .reversed()
+            .forEach { crates2[instruction[2]-1].push(it)}
+        }
+        return crates2.map { it.pop() }.joinToString("")
     }
 }
 
